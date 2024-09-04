8f95fa6d767d111dbd5d037686302a7ba39dcd62
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-09-04 15:35:50 +0300

    TASK-11 denemetask123

--- Program/button.py
+++ Program/button.py
@@ -6,7 +6,7 @@ class Button():
 		self.y_pos = pos[1] 
 		# self.text = self.font.render(self.text_input, True, self.base_color)   
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))    
-       
+        
 	def update(self, screen):    
 		if self.image is not None:   
 			screen.blit(self.image, self.rect) 
