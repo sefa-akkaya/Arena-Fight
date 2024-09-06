a820ad3a1d898983e81f0db1a3cea6586622b8eb
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-09-06 12:30:14 +0300

    TASK-11 asfdsf

--- Program/button.py
+++ Program/button.py
@@ -5,7 +5,7 @@ class Button():
 		self.x_pos = pos[0]       
 		self.y_pos = pos[1] 
 		# self.text = self.font.render(self.text_input, True, self .base_color)    
-		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))       
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))        
            
 	def update(self, screen):       
 		if self.image is not None:     
