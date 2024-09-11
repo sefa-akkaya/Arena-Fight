0ebe5a1e78917a4ca904e31647f05c8fb8aadc35
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-09-11 13:05:21 +0300

    HVLSN-29 fixed some stuff

---
+++ kotu_kod.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+class task:
+    def __init__(self, t, p, d, e):
+        self.t = t
+        self.p = p
+        self.d = d
+        self.e = e
+
+def sorting_things(t):
+    now = datetime.now()
+    def s(task):
+        tl = (task.d - now).total_seconds() / 3600
+        if tl == 0:
+            return 1000000  
+        return (task.p / tl) / task.e
+
+    t.sort(key=s, reverse=True)
+    return t
+
+tasks = [
+    task("report", 10, datetime.now() + timedelta(hours=5), 2),
+    task("slides", 7, datetime.now() + timedelta(hours=48), 4),
+    task("assignment", 8, datetime.now() + timedelta(hours=12), 6),
+    task("docs", 6, datetime.now() + timedelta(hours=24), 3)
+]
+
+x = sorting_things(tasks)
+for i in x:
+    print(i.t)
